bf4d73c10c01d350740166e9b8da18578af73dd2
pung8146 <pung8146@naver.com>
2025-05-08 07:01:17 +0900

    서버 컴포넌트 권장사항 추가 및 페이지 컴포넌트에 서버 전용 코드 삽입

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,9 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log("브라우저에서는 실행되지 않고 서버에서만 실행됩니다(터미널)");
+
+  const secretKey = "비밀번호를 넣어놔도 서버측에서만 실행됩니다";
+
   return <div className={styles.page}>인덱스 페이지</div>;
 }
