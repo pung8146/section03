8afdd2065ca64983e50319ffebe75060178dfd2d
pung8146 <pung8146@naver.com>
2025-05-14 06:08:31 +0900

    페이지 컴포넌트 수정: 클라이언트 컴포넌트를 추가하고 서버 전용 코드를 제거하여 구조 개선

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,9 +1,10 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("브라우저에서는 실행되지 않고 서버에서만 실행됩니다(터미널)");
-
-  const secretKey = "비밀번호를 넣어놔도 서버측에서만 실행됩니다";
-
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      <ClientComponent />
+    </div>
+  );
 }
