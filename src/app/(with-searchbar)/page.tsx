64a3ce404485008da11c52c3d8e8ef87dd9fdd5f
pung8146 <pung8146@naver.com>
2025-05-15 06:45:44 +0900

    인덱스 페이지 수정: 클라이언트 컴포넌트에 서버 컴포넌트를 추가하여 구조 개선

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,10 +1,13 @@
 import ClientComponent from "./client-component";
 import styles from "./page.module.css";
-
+import ServerComponent from "./server-component";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <ClientComponent />
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
     </div>
   );
 }
